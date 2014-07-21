40c710feeaa479586174a7ee449ce1826c3e692c
Per Digre <per@digre.com>
2014-07-21 20:52:28 +0200

    Moved castling logic

--- TheChessProject/src/no/pdigre/chess/engine/base/MKBlackStart.java
+++ TheChessProject/src/no/pdigre/chess/engine/base/MKBlackStart.java
@@ -1,6 +1,13 @@
 package no.pdigre.chess.engine.base;
 
+import no.pdigre.chess.engine.base.IConst.BITS;
+
 public class MKBlackStart extends MKBlack {
+	final static long CASTLING_BK = BITS.assemble(IConst.BK, IConst.BK_STARTPOS, IConst.BK_STARTPOS + 2, IConst.CANCASTLE_WHITE | IConst.SPECIAL);
+	final static long CASTLING_BQ = BITS.assemble(IConst.BK, IConst.BK_STARTPOS, IConst.BK_STARTPOS - 2, IConst.CANCASTLE_WHITE | IConst.SPECIAL);
+	final static long SIDE_BK = BITS.assemble(IConst.BK, IConst.BK_STARTPOS, IConst.BK_STARTPOS + 1, IConst.CANCASTLE_WHITE);
+	final static long SIDE_BQ = BITS.assemble(IConst.BK, IConst.BK_STARTPOS, IConst.BK_STARTPOS - 1, IConst.CANCASTLE_WHITE);
+
 	public MKBlackStart(int from) {
 		super(from);
 	}
@@ -12,14 +19,14 @@ public class MKBlackStart extends MKBlack {
 		if ((IConst.CBQ & gen.bb_piece) == 0
 				&& (gen.castling & IConst.CANCASTLE_BLACKQUEEN) != 0
 				&& !gen.pos.isCheckBlack()
-				&& !gen.pos.move(IBase.SIDE_BQ).isCheckBlack()) {
-			add(gen, IBase.CASTLING_BQ & gen.castling);
+				&& !gen.pos.move(SIDE_BQ).isCheckBlack()) {
+			add(gen, CASTLING_BQ & gen.castling);
 		}
 		if ((IConst.CBK & gen.bb_piece) == 0
 				&& (gen.castling & IConst.CANCASTLE_BLACKKING) != 0
 				&& !gen.pos.isCheckBlack()
-				&& !gen.pos.move(IBase.SIDE_BK).isCheckBlack()) {
-			add(gen, IBase.CASTLING_BK & gen.castling);
+				&& !gen.pos.move(SIDE_BK).isCheckBlack()) {
+			add(gen, CASTLING_BK & gen.castling);
 		}
 	}
 }
--- TheChessProject/src/no/pdigre/chess/engine/base/MKWhiteStart.java
+++ TheChessProject/src/no/pdigre/chess/engine/base/MKWhiteStart.java
@@ -1,9 +1,15 @@
 package no.pdigre.chess.engine.base;
 
+import no.pdigre.chess.engine.base.IConst.BITS;
+
 public class MKWhiteStart extends MKWhite {
+	final static long CASTLING_WK = BITS.assemble(IConst.WK, IConst.WK_STARTPOS, IConst.WK_STARTPOS + 2, IConst.CANCASTLE_BLACK | IConst.SPECIAL);
+	final static long CASTLING_WQ = BITS.assemble(IConst.WK, IConst.WK_STARTPOS, IConst.WK_STARTPOS - 2, IConst.CANCASTLE_BLACK | IConst.SPECIAL);
+	final static long SIDE_WK = BITS.assemble(IConst.WK, IConst.WK_STARTPOS, IConst.WK_STARTPOS + 1, IConst.CANCASTLE_BLACK);
+	final static long SIDE_WQ = BITS.assemble(IConst.WK, IConst.WK_STARTPOS, IConst.WK_STARTPOS - 1, IConst.CANCASTLE_BLACK);
+
 	public MKWhiteStart(int from) {
 		super(from);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -13,14 +19,14 @@ public class MKWhiteStart extends MKWhite {
 		if ((IConst.CWQ & gen.bb_piece) == 0
 				&& (gen.castling & IConst.CANCASTLE_WHITEQUEEN) != 0
 				&& !gen.pos.isCheckWhite()
-				&& !gen.pos.move(IBase.SIDE_WQ).isCheckWhite()) {
-			add(gen, IBase.CASTLING_WQ & gen.castling);
+				&& !gen.pos.move(SIDE_WQ).isCheckWhite()) {
+			add(gen, CASTLING_WQ & gen.castling);
 		}
 		if ((IConst.CWK & gen.bb_piece) == 0
 				&& (gen.castling & IConst.CANCASTLE_WHITEKING) != 0
 				&& !gen.pos.isCheckWhite()
-				&& !gen.pos.move(IBase.SIDE_WK).isCheckWhite()) {
-			add(gen, IBase.CASTLING_WK & gen.castling);
+				&& !gen.pos.move(SIDE_WK).isCheckWhite()) {
+			add(gen, CASTLING_WK & gen.castling);
 		}
 	}
 }
